297f0cb30c737ac2e16326bd69277db8784acd53
MS269 <ehddnr9797@gmail.com>
2021-09-29 00:24:34 +0900

    TS Setup

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,21 +1,34 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { darkModeVar, isLoggedInVar } from "../apollo";
 
-const Title = styled.h1`
-  color: ${(props) => props.theme.fontColor};
-`;
-
-const Container = styled.div``;
-
 function Login() {
+  const [big, setBig] = useState(false);
+
+  const toggleBig = (big: boolean) => setBig(!big);
+
   return (
     <Container>
-      <Title>Login</Title>
+      <Title big={big}>Login</Title>
       <button onClick={() => isLoggedInVar(true)}>Login Now!</button>
       <button onClick={() => darkModeVar(true)}>To dark</button>
       <button onClick={() => darkModeVar(false)}>To light</button>
+      <button onClick={() => toggleBig(big)}>
+        {big ? "Smaller" : "Bigger"}
+      </button>
     </Container>
   );
 }
+
+interface IContainerProps {
+  big: boolean;
+}
+
+const Title = styled.h1<IContainerProps>`
+  color: ${(props) => props.theme.fontColor};
+  font-size: ${(props) => (props.big ? "32px" : "16px")};
+`;
+
+const Container = styled.div``;
 
 export default Login;
--- src/styles.ts
+++ src/styles.ts
@@ -1,20 +1,20 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, DefaultTheme } from "styled-components";
 import reset from "styled-reset";
 
-export const lightTheme = {
-  fontColor: "#2c2c2c",
+export const lightTheme: DefaultTheme = {
   bgColor: "lightgray",
+  fontColor: "#2c2c2c",
 };
 
-export const darkTheme = {
-  fontColor: "lightgray",
+export const darkTheme: DefaultTheme = {
   bgColor: "#2c2c2c",
+  fontColor: "lightgray",
 };
 
 export const GlobalStyles = createGlobalStyle`
     ${reset}
 
     body {
-        background-color: ${(props: any) => props.theme.bgColor};
+        background-color: ${(props) => props.theme.bgColor};
     }
 `;
