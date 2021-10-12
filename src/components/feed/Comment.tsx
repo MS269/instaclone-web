f7a05f61747c23b103b0f818ee2c17ec2456dd66
MS269 <ehddnr9797@gmail.com>
2021-10-12 21:15:55 +0900

    Sanitize and Set Inner HTML

--- src/components/feed/Comment.tsx
+++ src/components/feed/Comment.tsx
@@ -1,3 +1,4 @@
+import sanitizeHtml from "sanitize-html";
 import styled from "styled-components";
 import { FatText } from "../sharedStyles";
 
@@ -10,13 +11,26 @@ const CommentContainer = styled.div``;
 
 const CommentCaption = styled.span`
   margin-left: 10px;
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 function Comment({ author, payload }: ICommentProps) {
+  const cleanedPayload = sanitizeHtml(
+    payload.replace(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g, "<mark>$&</mark>"),
+    { allowedTags: ["mark"] }
+  );
+
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption dangerouslySetInnerHTML={{ __html: cleanedPayload }} />
     </CommentContainer>
   );
 }
